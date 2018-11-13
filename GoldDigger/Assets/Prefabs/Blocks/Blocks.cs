9a6d8b2b6626bf41cd0879ecaf32f0770cd213b1
Dominik Reimer <reimerdo71515@th-nuernberg.de>
2018-11-13 20:59:24 +0100

    Added Blocks Class

---
+++ GoldDigger/Assets/Prefabs/Blocks/Blocks.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Block
+{
+    Dirt,
+    Stone
+}
+
+public abstract class Blocks : MonoBehaviour
+{
+
+    protected string name;
+    protected int hardness;
+    protected bool breakable = true;
+    protected int posX;
+    protected int posY;
+
+    public Blocks(string name, int hardness, int posX, int posY, bool breakable = true)
+    {
+        this.name = name;
+        this.hardness = hardness;
+        this.posX = posX;
+        this.posY = posY;
+        this.breakable = breakable;
+
+        GameObject DirtBlock = new GameObject(name);
+        DirtBlock.AddComponent<SpriteRenderer>();
+        SpriteRenderer sp = DirtBlock.GetComponent<SpriteRenderer>();
+        sp.sprite = Resources.Load<Sprite>("Sprites/Blocks/" + name);
+        DirtBlock.transform.position = new Vector2(posX, posY);
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void setTexture()
+    {
+        
+
+    }
+
+    void onBreak()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ GoldDigger/Assets/Prefabs/Blocks/DirtBlock.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class DirtBlock : Blocks
+{
+    public GameObject go;
+    public DirtBlock(string name, int hardness, int posX, int posY, bool breakable = true) 
+        : base(name, hardness, posX, posY, breakable)
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
