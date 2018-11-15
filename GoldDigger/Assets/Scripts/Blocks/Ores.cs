fdfea941a6c44d8da6774058441263fd2956112f
Dominik Reimer <reimerdo71515@th-nuernberg.de>
2018-11-15 12:15:50 +0100

    Created Ores Class

---
+++ GoldDigger/Assets/Scripts/Blocks/Ores.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Ores : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+  void onDestroy()
+  {
+    //DROP ERZE
+  }
+}
