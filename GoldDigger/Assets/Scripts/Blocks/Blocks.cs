f9622d9b84f3b5cf09b384dc7d32e48288042568
Dominik Reimer <reimerdo71515@th-nuernberg.de>
2018-11-15 12:12:31 +0100

    Added beginning of Worldgen

---
+++ GoldDigger/Assets/Scripts/Blocks/Blocks.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blocks : MonoBehaviour
+{
+    public string blockName;
+    public int hardness;
+    public bool breakable = true;
+
+    void Start()
+    {
+
+    }
+
+    void onBreak()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ GoldDigger/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MapGenerator : MonoBehaviour
+{
+
+  public GameObject GrassBlock;
+  public GameObject DirtBlock;
+  public GameObject StoneBlock;
+  public GameObject CopperBlock;
+  public GameObject CoalBlock;
+
+  int worldHeight;
+
+  // Use this for initialization
+  void Start()
+  {
+    //for(int i = 0; i <= 50; i++)
+    //{
+    //    for (int j = 0; j <= 10; j++)
+    //    {
+    //        Instantiate(Resources.Load("Prefabs/Blocks/DirtBlock", typeof(GameObject)), new Vector2(i, -j), Quaternion.identity);
+    //    }
+    //}
+    createGrassLayer(50, 10);
+    createStoneLayer1(50, 500);
+    
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void createStoneLayer1(int width, int height)
+  {
+    for (int i = 0; i < height; i++)
+    {
+      
+      for (int j = 0; j < width; j++)
+      {
+        GameObject active = null;
+
+        float rand = Random.Range(0f, 100f);
+        if (rand <= 4)
+        {
+          active = CoalBlock;
+        }
+        else if (rand <= 7)
+        {
+          active = CopperBlock;
+        }
+
+        if (active == null)
+        {
+          active = StoneBlock;
+        }
+        Instantiate(active, new Vector2(j, -i - worldHeight), Quaternion.identity);
+      }
+    }
+  }
+
+  void createGrassLayer(int width, int height)
+  {
+
+    for (int i = 0; i < height; i++)
+    {
+      GameObject active;
+      for (int j = 0; j < width; j++)
+      {
+        int rand = Random.Range(0, 100);
+        if (rand >= 5)
+        {
+          active = DirtBlock;
+        }
+        else
+        {
+          active = StoneBlock;
+        }
+        Instantiate(active, new Vector2(j, -i - worldHeight), Quaternion.identity);
+      }
+    }
+    worldHeight += height;
+  }
+}
