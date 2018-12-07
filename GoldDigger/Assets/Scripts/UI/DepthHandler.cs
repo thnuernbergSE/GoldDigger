a41b8ca61fd163c62a4ff9f5fd67649c11d8cdad
Dominik Reimer <dominik.reimer@web.com>
2018-12-07 12:27:00 +0100

    Beginning of UI Update

---
+++ GoldDigger/Assets/Scripts/UI/DepthHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class DepthHandler : MonoBehaviour
+{
+
+  Text text;
+
+	// Use this for initialization
+	void Start ()
+	{
+	  text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
