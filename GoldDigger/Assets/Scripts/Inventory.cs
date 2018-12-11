007b37138789d1b74f3d2e73bcd98f9148259c0d
Max-121 <44642857+Max-121@users.noreply.github.com>
2018-12-11 19:43:04 +0100

    pickaxe

---
+++ GoldDigger/Assets/Scripts/Inventory.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory<Space> : MonoBehaviour
+{
+    public Space Maxspace;
+
+    public Inventory(Space maxspace)
+    {
+        Maxspace = maxspace;
+    }
+    class KeyValueListe<ItemName, AmountOf>
+    {
+        class Item
+        {
+            public ItemName ItemName;
+            public AmountOf AmountOf;
+            public Item next;
+            public Item(ItemName itemName, AmountOf amountOf)
+            {
+                itemName = ItemName;
+                amountOf = AmountOf;
+            }
+        }
+        Item start, end;
+
+        public void Add()
+        { }
+
+        public void Remove()
+        { }
+
+
+    }
+}
