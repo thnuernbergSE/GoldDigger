b8dfe74c6667ca8b8872b5806ff6802bb7310656
Dominik Reimer <dominik.reimer@web.com>
2018-12-13 21:32:30 +0100

    Fixed SomeStuff

---
+++ GoldDigger/Assets/ShopHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopHandler : MonoBehaviour {
+
+  GameObject thinkingCloud;
+  // Use this for initialization
+  void Start()
+  {
+    thinkingCloud = GameObject.Find("thinkCloud");
+    thinkingCloud.SetActive(false);
+  }
+
+  // Update is called once per frame
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    thinkingCloud.SetActive(true);
+  }
+
+  void OnTriggerExit2D(Collider2D other)
+  {
+    thinkingCloud.SetActive(false);
+  }
+}
