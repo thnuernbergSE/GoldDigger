b739baecec38f279e7a72b26fe04b505857154fa
roli-rk <44464740+roli-rk@users.noreply.github.com>
2019-01-03 22:21:16 +0100

    Added TNT

---
+++ GoldDigger/Assets/ExplotionHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplotionHandler : MonoBehaviour {
+  int radius = 3;
+	// Use this for initialization
+	void Start () {
+    Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+    foreach (Collider2D nearbyObject in colliders)
+    {
+      if (nearbyObject == null || nearbyObject.gameObject.tag != "Blocks")
+      {
+        continue;
+      }
+      Destroy(nearbyObject.gameObject);
+    }
+  }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ GoldDigger/Assets/Scripts/Enemie/Explotion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explotion: MonoBehaviour
+{
+  public GameObject explostionEffect;
+
+  float countdown;
+
+  float radius = 1.0f;
+
+  bool hasExplodede = false;
+
+  public float force = 700f;
+
+  public void ReceiveDamage(int[] itemInfo)
+  {
+    explotion();
+  }
+
+  void explotion()
+  {
+    Instantiate(explostionEffect, transform.position, transform.rotation);
+  }
+
+
+}
+
