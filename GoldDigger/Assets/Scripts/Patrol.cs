762966ff2575375745ef1d492a4ee8722fcf59f3
Masa-99 <44638953+Masa-99@users.noreply.github.com>
2019-01-03 23:16:03 +0100

    Added beginning of bugs

---
+++ GoldDigger/Assets/Scripts/Patrol.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour {
+
+    public float SpitCooldown = 2f;
+
+    public float speed;
+
+    private float distance;
+
+    private float timeBtwShots;
+
+    public float startTimeShots;
+
+    private bool movingRight = true;
+
+    public bool GetMovingRight => movingRight;
+
+    public Transform wallDetection;
+
+    public GameObject spiderattack;
+    private Transform player;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Bug2").transform;
+
+        timeBtwShots = startTimeShots;
+    }
+
+    void Update()
+    {
+        SpitCooldown -= Time.deltaTime;
+
+        if(SpitCooldown <= 0)
+        {
+            Instantiate(spiderattack, transform.position, Quaternion.identity);
+        }
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D wallinfo = Physics2D.Raycast(wallDetection.position, Vector2.down, 2f );
+
+        
+        timeBtwShots = startTimeShots;
+
+        if (!wallinfo.collider)
+        {
+            if (movingRight)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+
+                movingRight = true;
+            }
+        }
+    }
+}
